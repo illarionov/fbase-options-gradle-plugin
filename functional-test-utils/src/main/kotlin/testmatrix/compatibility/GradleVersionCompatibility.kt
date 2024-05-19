2bf3799713fc0d3fad4bb3c0aaec9c90d7874963
Alexey Illarionov <alexey@0xdc.ru>
2024-05-19 05:13:15 +0300

    Remove common package from paths following Kotlin Code Conventions

--- functional-test-utils/src/main/kotlin/testmatrix/compatibility/GradleVersionCompatibility.kt
+++ functional-test-utils/src/main/kotlin/testmatrix/compatibility/GradleVersionCompatibility.kt
@@ -4,6 +4,8 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 
+@file:Suppress("MagicNumber")
+
 package ru.pixnews.gradle.fbase.test.functional.testmatrix.compatibility
 
 import ru.pixnews.gradle.fbase.test.functional.testmatrix.Version
---
+++ functional-test-utils/src/test/kotlin/Dummy.kt
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2024, the fbase-config-generator-gradle-plugin project authors and contributors.
+ * Please see the AUTHORS file for details.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
+@file:Suppress("FILE_CONTAINS_ONLY_COMMENTS", "FILE_NO_BLANK_LINE_BETWEEN_BLOCKS")
+
+// Workaround for https://youtrack.jetbrains.com/issue/KTIJ-27842
+package ru.pixnews.gradle.fbase.test.functional
