b306444b06aab4e0833829aa5d71275ab86c3d6f
Alexey Illarionov <alexey@0xdc.ru>
2024-07-12 01:24:33 +0300

    Add new versions to test matrix
    * AGP 8.6.0-beta01, 8.5.0, 8.4.2
    * Gradle 8.8, 8.9
    * Firebase BOM 33.1.2
    * Kotlin 2.0.0
    
    Removed tests with AGP 8.1.4, Firebase BOM 32.5.0

--- functional-test-utils/src/main/kotlin/testmatrix/VersionCatalog.kt
+++ functional-test-utils/src/main/kotlin/testmatrix/VersionCatalog.kt
@@ -1,16 +1,16 @@
 /*
- * Copyright (c) 2023, the fbase-config-generator-gradle-plugin project authors and contributors.
+ * Copyright (c) 2023-2024, the fbase-config-generator-gradle-plugin project authors and contributors.
  * Please see the AUTHORS file for details.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 
 package ru.pixnews.gradle.fbase.test.functional.testmatrix
 
-import ru.pixnews.gradle.fbase.test.functional.testmatrix.compatibility.AgpVersionCompatibility.AGP_8_4_0
+import ru.pixnews.gradle.fbase.test.functional.testmatrix.compatibility.AgpVersionCompatibility.AGP_8_5_0
 import ru.pixnews.gradle.fbase.test.functional.testmatrix.compatibility.AgpVersionCompatibility.getCompatibleAndroidApiLevel
-import ru.pixnews.gradle.fbase.test.functional.testmatrix.compatibility.FirebaseCompatibility.FIREBASE_BOM_33_0_0
-import ru.pixnews.gradle.fbase.test.functional.testmatrix.compatibility.GradleVersionCompatibility.GRADLE_8_7
-import ru.pixnews.gradle.fbase.test.functional.testmatrix.compatibility.KotlinVersionCompatibility.KOTLIN_2_0_0_RC3
+import ru.pixnews.gradle.fbase.test.functional.testmatrix.compatibility.FirebaseCompatibility.FIREBASE_BOM_33_1_2
+import ru.pixnews.gradle.fbase.test.functional.testmatrix.compatibility.GradleVersionCompatibility.GRADLE_8_9
+import ru.pixnews.gradle.fbase.test.functional.testmatrix.compatibility.KotlinVersionCompatibility.KOTLIN_2_0_0
 
 public data class VersionCatalog(
     val gradleVersion: Version,
@@ -26,14 +26,14 @@ public data class VersionCatalog(
     public companion object {
         public val ANDROIDX_CORE_VERSION: Version = Version(1, 13, 1)
         public fun getDefault(fbasePluginVersion: String): VersionCatalog {
-            val agpVersion = AGP_8_4_0
+            val agpVersion = AGP_8_5_0
             val compileTargetSdk = getCompatibleAndroidApiLevel(agpVersion)
             return VersionCatalog(
-                gradleVersion = GRADLE_8_7,
-                kotlinVersion = KOTLIN_2_0_0_RC3,
+                gradleVersion = GRADLE_8_9,
+                kotlinVersion = KOTLIN_2_0_0,
                 agpVersion = agpVersion,
                 androidxCore = ANDROIDX_CORE_VERSION,
-                firebaseVersion = FIREBASE_BOM_33_0_0,
+                firebaseVersion = FIREBASE_BOM_33_1_2,
                 compileSdk = compileTargetSdk,
                 targetSdk = compileTargetSdk,
                 fbasePluginVersion = fbasePluginVersion,
--- functional-test-utils/src/main/kotlin/testmatrix/compatibility/FirebaseCompatibility.kt
+++ functional-test-utils/src/main/kotlin/testmatrix/compatibility/FirebaseCompatibility.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, the fbase-config-generator-gradle-plugin project authors and contributors.
+ * Copyright (c) 2023-2024, the fbase-config-generator-gradle-plugin project authors and contributors.
  * Please see the AUTHORS file for details.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
@@ -10,6 +10,7 @@ import ru.pixnews.gradle.fbase.test.functional.testmatrix.Version
 
 // https://firebase.google.com/support/release-notes/android
 public object FirebaseCompatibility {
+    public val FIREBASE_BOM_33_1_2: Version = Version(33, 1, 2)
     public val FIREBASE_BOM_33_0_0: Version = Version(33, 0, 0)
     public val FIREBASE_BOM_32_8_1: Version = Version(32, 8, 1)
     public val FIREBASE_BOM_32_7_4: Version = Version(32, 7, 4)
